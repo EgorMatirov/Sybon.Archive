402e15d9d015797fa905958d6b7f3c78c60c065f
Mikhail Ivchenko <ematirov@gmail.com>
2018-03-01 21:03:54 +0400

    Fix config

--- Sybon.Archive/ArchiveSecurityConfiguration.cs
+++ Sybon.Archive/ArchiveSecurityConfiguration.cs
@@ -45,6 +45,21 @@ namespace Sybon.Archive
             
             public string Url => Configuration.GetValue<string>("Url");
         }
+
+        public class InfluxDbConfiguration
+        {
+            public InfluxDbConfiguration(IConfiguration configuration)
+            {
+                Configuration = configuration;
+            }
+        
+            private IConfiguration Configuration { get; }
+            
+            public string Url => Configuration.GetValue<string>("Url");
+            public string Password => Configuration.GetValue<string>("Password");
+            public string UserName => Configuration.GetValue<string>("UserName");
+            public string Database => Configuration.GetValue<string>("Database");
+        }
         
         public ArchiveSecurityConfiguration(IConfiguration configuration)
         {
@@ -58,20 +73,5 @@ namespace Sybon.Archive
         public SybonAuthConfiguration SybonAuth => new SybonAuthConfiguration(Configuration.GetSection("SybonAuth"));
         public InfluxDbConfiguration InfluxDb => new InfluxDbConfiguration(Configuration.GetSection("InfluxDb"));
         public string ApiKey => Configuration.GetValue<string>("ApiKey");
-    }
-
-    public class InfluxDbConfiguration
-    {
-        public InfluxDbConfiguration(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-        
-        private IConfiguration Configuration { get; }
-            
-        public string Url => Configuration.GetValue<string>("Url");
-        public string Password => Configuration.GetValue<string>("Password");
-        public string UserName => Configuration.GetValue<string>("UserName");
-        public string Database => Configuration.GetValue<string>("Database");
     }
 }
